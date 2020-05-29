214308578511a4abdc1b30e97021dbe750aa498b
art-cy <64624033+art-cy@users.noreply.github.com>
2020-05-29 15:59:49 +0700

    Add files via upload

---
+++ antiDSC.py
@@ -0,0 +1,10 @@
+import requests
+import os
+import time
+#############
+time.sleep(1)
+os.system("clear")
+os.system("figlet -f mono12 DSC noob | lolcat")
+time.sleep(1)
+print("\033[1;91mCreat By J4Y youtube H4CK H33")
+#########################
